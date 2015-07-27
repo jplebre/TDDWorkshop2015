2189416687a9330ee1a9784b25d9ebe607bbf7e0
jplebre <jplebre@gmail.com>
2015-07-27 19:32:14 +0100

    some changes

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -10,6 +10,21 @@ namespace FizzBuzz
 
             for (int i = 1; i <= 100; i++)
             {
+                bool testFizz = i % 3 == 0;
+                bool testBuzz = i % 5 == 0;
+
+                switch (true)
+                {
+                    case testFizz: 
+                        output += "Fizz";
+                    case testBuzz:
+                        output += "Buzz";
+                    default:
+                        output += ",";
+                }
+                        
+                        
+                            
                 if (i % 3 == 0)
                 {
                     output += "Fizz";
